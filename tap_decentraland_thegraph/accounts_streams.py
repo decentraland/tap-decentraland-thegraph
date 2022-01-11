27b3e97f855362c92b1f8a60ed74383087882292
Rodrigo Antonioli <rodrigo@decentraland.org>
2022-01-11 16:39:31 -0300

    generate child classes

--- tap_decentraland_thegraph/accounts_streams.py
+++ tap_decentraland_thegraph/accounts_streams.py
@@ -9,14 +9,8 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 from tap_decentraland_thegraph.client import DecentralandTheGraphCompleteObjectStream
 
 
-class ETHAccountsStream(DecentralandTheGraphCompleteObjectStream):
-    name = "accounts_eth"
-
-    @property
-    def url_base(self) -> str:
-        """Return the API URL root, configurable via tap settings."""
-        return self.config["eth_accounts_url"]
-
+class AccountsStream(DecentralandTheGraphCompleteObjectStream):
+    
     primary_keys = ["id"]
     object_returned = 'accounts'
     
@@ -46,56 +40,28 @@ class ETHAccountsStream(DecentralandTheGraphCompleteObjectStream):
         th.Property("id", th.StringType, required=True),
         th.Property("address", th.StringType),
         th.Property("isCommitteeMember", th.StringType),
-        th.Property("totalCurations", th.StringType),
-        th.Property("sales", th.StringType),
-        th.Property("purchases", th.StringType),
+        th.Property("totalCurations", th.IntegerType),
+        th.Property("sales", th.IntegerType),
+        th.Property("purchases", th.IntegerType),
         th.Property("spent", th.StringType),
         th.Property("earned", th.StringType),
         th.Property("royalties", th.StringType),
     ).to_dict()
     
 
-class PolygonAccountsStream(DecentralandTheGraphCompleteObjectStream):
+class ETHAccountsStream(AccountsStream):
+    name = "accounts_ethereum"
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config["eth_accounts_url"]
+
+
+class PolygonAccountsStream(AccountsStream):
     name = "accounts_polygon"
 
     @property
     def url_base(self) -> str:
         """Return the API URL root, configurable via tap settings."""
-        return self.config["polygon_accounts_url"]
-
-    primary_keys = ["id"]
-    object_returned = 'accounts'
-    
-    query = """
-        query ($offset: Int!)
-        {
-            accounts(
-                first: 1000,
-                skip: $offset,
-                orderBy:spent,
-                orderDirection:desc
-            ) {
-                id
-                address
-                isCommitteeMember
-                totalCurations
-                sales
-                purchases
-                spent
-                earned
-                royalties
-            }
-        }
-        """    
-
-    schema = th.PropertiesList(
-        th.Property("id", th.StringType, required=True),
-        th.Property("address", th.StringType),
-        th.Property("isCommitteeMember", th.StringType),
-        th.Property("totalCurations", th.StringType),
-        th.Property("sales", th.StringType),
-        th.Property("purchases", th.StringType),
-        th.Property("spent", th.StringType),
-        th.Property("earned", th.StringType),
-        th.Property("royalties", th.StringType),
-    ).to_dict()
+        return self.config["polygon_api_url"]
