d2b5f0642b5bf36a2f54082bc403436dfcd81fd3
Rodrigo Antonioli <rodrigo@decentraland.org>
2022-01-25 13:28:02 +0100

    stringtype -> objecttype

--- tap_decentraland_thegraph/sales_streams.py
+++ tap_decentraland_thegraph/sales_streams.py
@@ -58,11 +58,13 @@ class ETHSalesStream(DecentralandTheGraphCompleteObjectStream):
         th.Property("feesCollectorCut", th.StringType),
         th.Property("feesCollector", th.StringType),
         th.Property("royaltiesCut", th.StringType),
+        th.Property("royaltiesCollector", th.StringType),
         th.Property("item", th.StringType),
         th.Property("nft", th.StringType),
         th.Property("timestamp", th.StringType),
         th.Property("txHash", th.StringType),
         th.Property("searchTokenId", th.StringType),
+        th.Property("searchItemId", th.StringType),
         th.Property("searchContractAddress", th.StringType)
     ).to_dict()
     
@@ -97,7 +99,18 @@ class PolygonSalesStream(DecentralandTheGraphCompleteObjectStream):
             royaltiesCut
             royaltiesCollector
             item
+            {
+                id
+                blockchainId
+                itemType
+            }
             nft
+                [
+                    id
+                    tokenId
+                    contractAddress
+                    itemBlockchainId
+                ]
             timestamp
             txHash
             searchTokenId
@@ -116,13 +129,25 @@ class PolygonSalesStream(DecentralandTheGraphCompleteObjectStream):
         th.Property("feesCollectorCut", th.StringType),
         th.Property("feesCollector", th.StringType),
         th.Property("royaltiesCut", th.StringType),
-        th.Property("item", th.StringType),
-        th.Property("nft", th.StringType),
+        th.Property("royaltiesCollector", th.StringType),
+        th.Property("item", th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("blockchainId", th.StringType),
+            th.Property("itemType", th.StringType)
+        )),
+        th.Property("nft", th.ObjectType(
+            th.Property("id", th.StringType),
+            th.Property("tokenId", th.StringType),
+            th.Property("contractAddress", th.StringType),
+            th.Property("itemBlockchainId", th.StringType)
+        )),
         th.Property("timestamp", th.StringType),
         th.Property("txHash", th.StringType),
         th.Property("searchTokenId", th.StringType),
+        th.Property("searchItemId", th.StringType),
         th.Property("searchContractAddress", th.StringType)
     ).to_dict()
+
 
 
 
