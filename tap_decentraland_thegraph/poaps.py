eaa57706ee44fba5541d6e4178ee4ab5d04fd26f
Pablo Seibelt <pabloseibelt@sicarul.com>
2022-04-04 07:19:10 -0300

    Upgrade singer sdk and switch to paginated api

--- tap_decentraland_thegraph/poaps.py
+++ tap_decentraland_thegraph/poaps.py
@@ -1,5 +1,7 @@
 """Stream type classes for tap-decentraland-thegraph."""
 
+import logging
+from datetime import datetime
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 
@@ -59,7 +61,9 @@ class PoapsXdai(DecentralandTheGraphPolygonStream):
 
 class PoapsMetadata(BaseAPIStream):
     name = "poaps_metadata"
-    path = "/events"
+    path = "/paginated-events"
+
+    RESULTS_PER_PAGE = 500
 
     @property
     def url_base(self) -> str:
@@ -67,13 +71,27 @@ class PoapsMetadata(BaseAPIStream):
         return self.config["poaps_details_url"]
 
     primary_keys = ['id']
+    replication_key = 'start_date'
+    replication_method = "INCREMENTAL"
+    is_sorted = True
+    records_jsonpath: str = "$.items[*]"
+    next_page_token_jsonpath: str = "$.items[-1:].start_date"
+    
+    def get_url_params(
+        self,
+        context: Optional[dict],
+        next_page_token: Optional[Any] = None
+    ) -> Dict[str, Any]:
+        next_timestamp = datetime(2000,1,1)
+        if next_page_token:
+            next_timestamp = datetime.strptime(next_page_token, '%d-%b-%Y')
+        self.logger.info(f"Time: {next_timestamp}")
+        return {"limit": self.RESULTS_PER_PAGE, "from_date": next_timestamp.strftime("%Y-%m-%dT%H:%M:%Sz"), "sort_field": "start_date", "sort_dir":"asc"}
 
-    def parse_response(self, response) -> Iterable[dict]:
-        """Parse Tiles rows"""
-
-        data =response.json()
-        for t in data:
-            yield t
+    def post_process(self, row: dict, context: Optional[dict] = None) -> dict:
+        """Add hash"""
+        row['start_date'] = datetime.strptime(row['start_date'], '%d-%b-%Y')
+        return row
 
     schema = th.PropertiesList(
         th.Property("id", th.IntegerType, required=True),
@@ -85,10 +103,9 @@ class PoapsMetadata(BaseAPIStream):
         th.Property("city", th.StringType),
         th.Property("description", th.StringType),
         th.Property("year", th.IntegerType),
-        th.Property("start_date", th.StringType),
+        th.Property("start_date", th.DateTimeType),
         th.Property("end_date", th.StringType),
         th.Property("expiry_date", th.StringType),
-        th.Property("created_date", th.StringType),
         th.Property("from_admin", th.BooleanType),
         th.Property("virtual_event", th.BooleanType),
         th.Property("event_template_id", th.IntegerType),
